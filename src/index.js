562e59e648ee9f19e6fff5cc20fb980e0f3551a9
Jian Lv <jian.lv@rea-group.com>
2019-02-14 12:51:14 +0800

    Add post mutation for Link

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews clone.`,
     feed: () => links
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    }
   }
 }
 
